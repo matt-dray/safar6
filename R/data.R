fbdbf061c654589c0cae5b5ba1f7b5eecc46c90c
Matt Dray <mwdray@gmail.com>
2021-01-02 17:50:04 +0000

    Update pokemon dataset

--- R/data.R
+++ R/data.R
@@ -3,15 +3,16 @@
 #' A dataset containing the Pokemon you can encounter in the Safari Zone in
 #' Pokemon Blue and their statistics.
 #'
-#' @format A data frame with 10 rows and 9 variables:
+#' @format A data frame with 10 rows and 10 variables:
 #' \describe{
 #'   \item{game}{The Pokemon generation 1 game variant. \code{BLUE} only for now.}
 #'   \item{area}{The area of the Safari Zone (\code{CENTER} only for now)}
 #'   \item{tile}{The terrain on which the player is standing (\code{GRASS} only for now).}
 #'   \item{species}{The encounter Pokemon's species.}
 #'   \item{level}{The encounter Pokemon's level.}
-#'   \item{catch_base}{The base catch rate for the species.}
+#'   \item{hp_base}{The base speed for the species.}
 #'   \item{speed_base}{The base speed for the species.}
+#'   \item{catch_base}{The base catch rate for the species.}
 #'   \item{slot}{Encounter rate slot, which determines \code{encounter_rate}}
 #'   \item{encounter_rate}{Chance of encounter.}
 #' }
