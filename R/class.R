8336c1d575f609d666f6fb701a719c380bd61234
Matt Dray <mwdray@gmail.com>
2021-01-02 15:19:48 +0000

    Add encounter while loop

--- R/class.R
+++ R/class.R
@@ -115,12 +115,98 @@ SafariZone <- R6::R6Class("SafariZone",
       # Encounter
       if (sample(0:255, 1) < 30) {  # base encounter rate is 30 in Safari Zone
 
-        cat("Wild", self$pkmn_sp, "appeared!")
+        # Select species/level by encounter rate
+        encounter_pkmn <-
+          dplyr::slice_sample(pokemon, weight_by = encounter_rate)
 
-        # choose a pokemon from the lookup, set fields
-        # initiate routine for player action
-        # initiate routine for pokemon action
-        # cease if run away, reinitiate player action otherwise
+        # Print encounter details
+        cat("Wild", encounter_pkmn$species,
+            paste0("L", encounter_pkmn$level), "appeared!\n")
+
+        ## code below should be in a while loop so that you get player turn
+        ## followed by pokemon turn until you run, it's caught or it runs
+
+        encounter_active <- TRUE  # encounter is active to start
+
+        while (encounter_active == TRUE) {
+
+          # # Adjust anger and eating status at start of turn
+          # if (private$pkmn_angry > 0) {
+          #   cat("Wild [x] is angry!")
+          #   self$pkmn_angry <- self$pkmn_angry - 1
+          #   if (self$pkmn_angry == 0) {
+          #     self$pkmn_c <- self$pkmn_base_c
+          #   }
+          # } else if (self$pkmn_eating > 0) {
+          #   cat("Wild [x] is eating!")
+          #   self$pkmn_eating <- self$pkmn_eating - 1
+          # }
+
+          # Ask player to choose from options
+          player_action <- readline(
+            paste0("BALLx", self$balls, " (1), BAIT (2), ROCK (3), RUN (4)? "))
+
+          # Ifelse chain dependent on player selection
+          if (player_action %in% c("BALL", "1")) {  # throw Safari Ball
+
+            # Reduce balls by 1
+            cat("BLUE used SAFARI BALL!")
+            self$balls <- self$balls - 1
+
+            # # Calculate catch chance
+            # Rstar <- sample(0:150, 1)  # ball RNG
+            # F1 <- (private$pkmn_hp * 255) / 12  # HP factor
+            # F2 <- max(private$pkmn_hp / 4, 1)
+            # F3 <- min(F1 / F2, 255)
+            #
+            # # Fail
+            # if (private$pkmn_catch_base < Rstar) {
+            #   cat("Darn! The POKeMON broke free!\nWild",
+            #       encounter_pkmn$species, "ran!")
+            # }
+            #
+            # # Second chance
+            # R2 <- sample(0:255, 1)
+            # if (R2 <= F1) {
+            #   cat("caught")
+            #   private$captures <- private$captures + 1
+            # }
+
+          } else if (player_action %in% c("BAIT", "2")) {  # throw bait
+
+            cat("BLUE threw some BAIT.\nWild",
+                encounter_pkmn$species, "is eating!")
+
+            # self$pkmn_catch <- floor(private$pkmn_catch_base / 2)
+            # self$pkmn_eating <- private$pkmn_eating + sample(1:5, 1)
+            # self$pkmn_angry <- 0
+
+          } else if (player_action %in% c("ROCK", "3")) {  # throw rock
+
+            cat("BLUE threw a ROCK.\nWild", encounter_pkmn$species, "is angry!")
+
+            # private$pkmn_catch <- min(private$pkmn_catch_base * 2, 255)
+            # private$pkmn_angry <- private$pkmn_angry + sample(1:5, 1)
+            # private$pkmn_eating <- 0
+
+          } else if (player_action %in% c("RUN", "4")) {  # exit encounter
+            cat("Got away safely!")
+            encounter_active <- FALSE
+          }
+
+          # # Wild Pokemon's turn
+          # X <- (self$pkmn_speed %% 256) * 2
+          # if (X > 255) {
+          #   cat("Wild", encounter_pkmn$species, "ran away!")
+          # }
+          # if (self$pkmn_angry > 0) { X <- min(X * 2, 255) }
+          # if (self$pkmn_eating > 0) { X <- X / 4 }
+          # R <- sample(0:255, 1)
+          # if (R < X) {
+          #   cat("Wild", encounter_pkmn$species, "ran away!")
+          # }
+
+        }
 
       }
 
